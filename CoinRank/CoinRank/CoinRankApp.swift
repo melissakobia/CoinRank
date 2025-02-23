3d5c7dce1b8d2ddd4e2a55b3956f3a54b907eb79
melissakobia <melissakobia1997@gmail.com>
2025-02-23 20:15:59 +0300

    Initial commit

---
+++ CoinRank/CoinRank/CoinRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinRankApp.swift
+//  CoinRank
+//
+//  Created by Muthoni on 23/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
